b42a233a7cd9de9b35efc877a684e60776ff9cf6
tofublork <rebornbreathing@gmail.com>
2023-11-18 04:00:27 +0800

    added receipt checking

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const expo = new Expo();
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
+// function extractExpoPushTokenFromReceipt(details) {
+//     return details.expoPushToken; 
+// }
+
 app.post('/send-push-notifications', async (req, res) => {
     try {
         // Extract notificationData from the request
@@ -16,7 +20,6 @@ app.post('/send-push-notifications', async (req, res) => {
         console.log(req.body);
 
         // Extract push tokens and message from notificationData
-        //const { expoPushTokensList, message } = notificationData;
         const { to, title, body, data } = notificationData;
 
         // Create the messages that you want to send to clients
@@ -36,18 +39,8 @@ app.post('/send-push-notifications', async (req, res) => {
                 body: body,
                 data: data,
             });
-
-            // // Construct a message
-            // messages.push({
-            //     to: pushToken,
-            //     sound: 'default',
-            //     body: 'This is a test notification',
-            //     data: { withSome: 'data' },
-            // });
         }
 
-        // The rest of your push notification sending logic...
-        // (Same as in your original code)
         // The Expo push notification service accepts batches of notifications so
         // that you don't need to send 1000 requests to send 1000 notifications. We
         // recommend you batch your notifications to reduce the number of requests
@@ -72,6 +65,57 @@ app.post('/send-push-notifications', async (req, res) => {
                     console.error(error);
                 }
             }
+
+            // Handle receipts after sending notifications
+            let receiptIds = [];
+            for (let ticket of tickets) {
+                // NOTE: Not all tickets have IDs; for example, tickets for notifications
+                // that could not be enqueued will have error information and no receipt ID.
+                if (ticket.id) {
+                    receiptIds.push(ticket.id);
+                }
+            }
+        })();
+
+        let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+        (async () => {
+            // Like sending notifications, there are different strategies you could use
+            // to retrieve batches of receipts from the Expo service.
+            for (let chunk of receiptIdChunks) {
+                try {
+                    let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+                    console.log(receipts);
+
+                    // The receipts specify whether Apple or Google successfully received the
+                    // notification and information about an error, if one occurred.
+                    for (let receiptId in receipts) {
+                        let { status, message, details } = receipts[receiptId];
+                        if (status === 'ok') {
+                            continue;
+                        } else if (status === 'error') {
+                            console.error(
+                                `There was an error sending a notification: ${message}`
+                            );
+                            if (details && details.error) {
+                                // The error codes are listed in the Expo documentation:
+                                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                                // You must handle the errors appropriately.
+                                console.error(`The error code is ${details.error}`);
+
+                                // Handle DeviceNotRegistered
+                                // if (details.error === 'DeviceNotRegistered') {
+                                //     const expoPushToken = extractExpoPushTokenFromReceipt(details);
+
+                                //     // Modify this part based on how you want to handle DeviceNotRegistered error
+                                //     console.log(`DeviceNotRegistered for ExpoPushToken: ${expoPushToken}`);
+                                // }
+                            }
+                        }
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
         })();
 
         res.status(200).json({ success: true, message: 'Push notifications sent successfully' });
@@ -82,5 +126,5 @@ app.post('/send-push-notifications', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+    //console.log(`Server is listening at http://localhost:${port}`);
 });
